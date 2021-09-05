702f5f03d49dbc861e23d10887f8aa17a11f445e
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2021-09-05 17:57:48 +0800

    初步分析

---
+++ Python/初步分析.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.sans-serif'] = ['Taipei Sans TC Beta']
+np1 = np.genfromtxt('D:/Database/Python_CKHS/Python_CKHS/資料檔案/總共事故數.csv', delimiter=',')
+np2 = np.genfromtxt('D:/Database/Python_CKHS/Python_CKHS/資料檔案/死亡車禍總數.csv', delimiter=',')
+a = np.linspace(102, 109, 8)
+plt.plot(a, np1)
+plt.plot(a, np2)
+plt.xlabel('年分')
+plt.ylabel('事故數')
+plt.title('初步分析表')
+plt.grid()
+plt.legend(loc='upper right')
+plt.show()
