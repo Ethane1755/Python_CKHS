e0b74bc80e4f32dd6e6f5d94266f8b3e1c27daaf
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2021-09-05 18:59:28 +0800

    重作

--- Python/109.py
+++ Python/109.py
@@ -2,7 +2,7 @@ import pandas as pd
 import folium
 from folium import plugins
 
-data = pd.read_csv('D:/Database/建中/Python_CKHS/Python_CKHS/資料檔案/地圖資料/1091.csv', encoding='ansi',  delimiter=',')
+data = pd.read_csv('D:/Database/Python_CKHS/Python_CKHS/資料檔案/地圖資料/109.csv', encoding='ansi',  delimiter=',')
 park_map = folium.Map(location=[data['X'].mean(), data['Y'].mean()], zoom_start=10, control_scale=True)
 marker_cluster = plugins.MarkerCluster().add_to(park_map)
 
--- Python/110.py
+++ Python/110.py
@@ -2,10 +2,10 @@ import pandas as pd
 import folium
 from folium import plugins
 
-data = pd.read_csv('D:/Database/Python/資料檔案/地圖資料/110.csv', encoding='ansi',  delimiter=',')
-park_map = folium.Map(location=[data['Y'].mean(), data['X'].mean()], zoom_start=10, control_scale=True)
+data = pd.read_csv('D:/Database/Python_CKHS/Python_CKHS/資料檔案/地圖資料/110.csv', encoding='ansi',  delimiter=',')
+park_map = folium.Map(location=[data['X'].mean(), data['Y'].mean()], zoom_start=10, control_scale=True)
 marker_cluster = plugins.MarkerCluster().add_to(park_map)
 
 for name,row in data.iterrows():
-    folium.Marker(location=[row["Y"], row["X"]], tooltip=[row['Z']]).add_to(marker_cluster)
+    folium.Marker(location=[row["X"], row["Y"]], tooltip=[row['Z']]).add_to(marker_cluster)
 park_map.save('park_map2.html')
